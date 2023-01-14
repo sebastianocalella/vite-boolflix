8c9bd130c56ef6de0c3608fdab586f8f06ddf940
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-14 15:40:09 +0100

    implement API request in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+    import axios from 'axios';
+
     import AppHeader from './components/AppHeader.vue';
     import AppMain from './components/AppMain.vue'
 
@@ -6,6 +8,25 @@
         components: {
             AppHeader,
             AppMain,
+        },
+        methods:{
+            getFilmList(){
+                axios.get('https://api.themoviedb.org/3/search/movie',{
+                    params:{
+                        api_key: 'dcba638d378e189477e8023df53d6ccf',
+                        query: 'ritorno+al+futuro'
+                    },
+                })
+                    .then((response)=>{
+                        console.log(response.data.results);
+                    })
+                    .catch(function (error) {
+                    console.log(error);
+                })
+            }
+        },
+        created(){
+            this.getFilmList();
         }
     }
 </script>
